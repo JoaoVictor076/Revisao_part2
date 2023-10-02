b2d2ea15006e121ad8e0becc3bbf8d8d456eba50
JoaoVictor076 <115574299+JoaoVictor076@users.noreply.github.com>
2023-10-01 23:25:21 -0300

    Add files via upload

---
+++ Revisao.Domain/Entities/JogoMegaSena.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao.Domain.Entities
+{
+    public class JogoMegaSena
+    {
+        public JogoMegaSena(string nome, string cpf, int primeiro_numero, int segundo_numero, int terceiro_numero, int quarto_numero, int quinto_numero, int sexto_numero, DateTime dataDoJogo)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            this.primeiro_numero = primeiro_numero;
+            this.segundo_numero = segundo_numero;
+            this.terceiro_numero = terceiro_numero;
+            this.quarto_numero = quarto_numero;
+            this.quinto_numero = quinto_numero;
+            this.sexto_numero = sexto_numero;
+            DataDoJogo = dataDoJogo;
+        }
+
+        [MinLength(3, ErrorMessage = "Minimo de 3 letras.")]
+        [MaxLength(255, ErrorMessage = "maximo ....")]
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int primeiro_numero { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int segundo_numero { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int terceiro_numero { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int quarto_numero { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int quinto_numero { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int sexto_numero { get; set; }
+        public DateTime DataDoJogo { get; set; }
+    }
+}
---
+++ Revisao_part2/Revisao.API/Controllers/MegaSenaController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Revisao.Application.Interfaces;
+using Revisao.Application.Services;
+using Revisao.Application.ViewModels.Request;
+using Revisao.Domain.Entities;
+
+namespace Revisao.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MegaSenaController : Controller
+    {
+        private readonly IMegaSenaService _jogoService;
+
+        public MegaSenaController(IMegaSenaService jogoService)
+        {
+            _jogoService = jogoService;
+        }
+
+        [HttpPost(Name = "AdicionarJogos")]
+        public IActionResult Post(NovoJogoViewModel novoJogoViewModel)
+        {
+            if (novoJogoViewModel.primeiro_numero != novoJogoViewModel.segundo_numero && novoJogoViewModel.primeiro_numero != novoJogoViewModel.terceiro_numero
+            && novoJogoViewModel.primeiro_numero != novoJogoViewModel.quarto_numero && novoJogoViewModel.primeiro_numero != novoJogoViewModel.quinto_numero
+            && novoJogoViewModel.primeiro_numero != novoJogoViewModel.sexto_numero
+
+            && novoJogoViewModel.segundo_numero != novoJogoViewModel.terceiro_numero && novoJogoViewModel.segundo_numero != novoJogoViewModel.quarto_numero
+            && novoJogoViewModel.segundo_numero != novoJogoViewModel.quinto_numero && novoJogoViewModel.segundo_numero != novoJogoViewModel.sexto_numero
+
+            && novoJogoViewModel.terceiro_numero != novoJogoViewModel.quarto_numero && novoJogoViewModel.terceiro_numero != novoJogoViewModel.quinto_numero
+            && novoJogoViewModel.terceiro_numero != novoJogoViewModel.sexto_numero
+
+            && novoJogoViewModel.quarto_numero != novoJogoViewModel.quinto_numero && novoJogoViewModel.terceiro_numero != novoJogoViewModel.sexto_numero
+
+            && novoJogoViewModel.quinto_numero != novoJogoViewModel.sexto_numero)
+            {
+                _jogoService.AdicionarJogos(novoJogoViewModel);
+                return Ok("Jogo Registrado com sucesso!");
+            }
+            else
+            {
+                return BadRequest("Dados incorretos, existem números repetidos no jogo");
+            }
+        }
+
+
+        [HttpGet(Name = "ObterTodosOsJogos")]
+        public IEnumerable<JogoMegaSenaViewModel> ObterNumeros()
+        {
+            string json = System.IO.File.ReadAllText(@"FileJsonData//jogosMega.json");
+            IEnumerable<JogoMegaSenaViewModel> pegarNumeros = JsonConvert.DeserializeObject<IEnumerable<JogoMegaSenaViewModel>>(json);
+            return pegarNumeros;
+        }
+    }
+}
+
+
+
+
+
